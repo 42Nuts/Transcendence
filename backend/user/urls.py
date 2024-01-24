eb0dddf444e0f5b370e968d532a33ec6dd3be7f8
gitubanana <ptyolo666@gmail.com>
2024-01-24 16:31:00 +0900

    Test: user 정보를 가져오는 API 호출해서 email 띄워주기
    - access token 사용
    Co-authored-by: PEAKY BLINDERS <GCgang@users.noreply.github.com>

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import login, home, nickname
+from .views import login, home, nickname, get_your_email
 
 urlpatterns = [
     path('', login),
     path('home/', home),
     path('nickname/', nickname),
+    path('get_your_email/', get_your_email),
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,7 +1,8 @@
 import requests
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponse
 
+access_token = ''
 
 def login(request):
     return render(request, "loginPage.html")
@@ -11,9 +12,17 @@ def home(request):
     return render(request, "homePage.html")
 
 
+def get_your_email(request):
+    url = f'https://api.intra.42.fr/v2/me?access_token={ access_token }'
+    print(url)
+    response = requests.get(url)
+    data = response.json()
+    return HttpResponse(data['email'])
+
 def nickname(request):
+    global access_token
+
     authorization_code = request.GET['code']
-    print('code', authorization_code)
     if authorization_code is None:
         return Http404()
 
@@ -27,13 +36,14 @@ def nickname(request):
     #     'response_type': 'code'
     # }
     # response = requests.post(url, data=data)
-    url = (f'https://api.intra.42.fr/oauth/token?grant_type=authorization_code&client_secret=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&client_id=u-s4t2ud-d10a9ce21bddf5c5122891fa28175e899c5994149a2c95ab9178de72cb1eb491&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fnickname&response_type=code&code={authorization_code}')
+    url = (
+        f'https://api.intra.42.fr/oauth/token?grant_type=authorization_code&client_secret=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&client_id=u-s4t2ud-d10a9ce21bddf5c5122891fa28175e899c5994149a2c95ab9178de72cb1eb491&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fnickname&response_type=code&code={authorization_code}')
     print(url)
     response = requests.post(url)
-    print(response)
     if response.status_code == 200:
         data = response.json()
-        print(data)
+        access_token = data['access_token']
+        print(access_token)
     else:
         print('error 입니다!')
     return render(request, "nicknamePage.html")
