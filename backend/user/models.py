629c9c1cdc7314a095ebcef02c6f77a321957d86
gitubanana <ptyolo666@gmail.com>
2024-01-25 14:19:18 +0900

    Feat: User 모델 생성
    Co-authored-by: PEAKY BLINDERS <GCgang@users.noreply.github.com>

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    access_token = models.TextField()
+    refresh_token = models.TextField()
+    user_state = models.BooleanField(default=True)
+    nickname = models.CharField(max_length=10)
+    profile_image_url = models.ImageField(
+        upload_to='images path', default='default image path')
+    background_color = models.CharField(max_length=20)
+    tier = models.IntegerField(default=0)
+    tier_image_url = models.ImageField(
+        upload_to='images path', default='default image path')
+    wins = models.IntegerField(default=0)
+    loses = models.IntegerField(default=0)
+    dark_mode = models.BooleanField(default=False)
+    game_skin_image_url = models.ImageField(
+        upload_to='images path', default='default image path')
+    follower_count = models.IntegerField(default=0)
+    following_count = models.IntegerField(default=0)
