137931bb4535024d6bb596eceaf8ecd51c80496a
GCgang <powm97@gmail.com>
2024-01-30 14:14:34 +0900

    Fix: User 모델 속성 수정
    Co-authored-by: taeypark <ptyolo666@gmail.com>

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,28 +1,30 @@
 from django.db import models
+from config.settings import DEFAULT_GAME_SKIN_IMAGE_URL, DEFAULT_TIER_IMAGE_URL, DEFAULT_PROFILE_IMAGE_URL
+
 
 class User(models.Model):
-    email = models.EmailField()
-    access_token = models.TextField()
-    refresh_token = models.TextField()
+    email = models.EmailField(unique=True)
+    access_token = models.TextField(blank=False)
+    refresh_token = models.TextField(blank=False)
     user_state = models.BooleanField(default=True)
     nickname = models.CharField(max_length=10)
     profile_image_url = models.ImageField(
-        upload_to='images path', default='default image path')
-    background_color = models.CharField(max_length=20)
+        upload_to='profile/', default=DEFAULT_PROFILE_IMAGE_URL)
+    background_color = models.CharField(max_length=20, default='black')
     tier = models.IntegerField(default=0)
-    tier_image_url = models.ImageField(
-        upload_to='images path', default='default image path')
+    tier_image_url = models.CharField(
+        max_length=255, default=DEFAULT_TIER_IMAGE_URL)
     wins = models.IntegerField(default=0)
     loses = models.IntegerField(default=0)
     dark_mode = models.BooleanField(default=False)
-    game_skin_image_url = models.ImageField(
-        upload_to='images path', default='default image path')
+    game_skin_image_url = models.CharField(
+        max_length=255, default=DEFAULT_GAME_SKIN_IMAGE_URL)
     follower_count = models.IntegerField(default=0)
     following_count = models.IntegerField(default=0)
     following = models.ManyToManyField(
         "self",
         verbose_name="팔로우 중인 사용자들",
-        related_name="followers", # 역방향 user1.following / user2.followers
+        related_name="followers",  # 역방향 user1.following / user2.followers
         symmetrical=False,
-        through="relationships.Relationship",
+        through="relationships.Relationship"
     )
--- backend/user/tests.py
+++ backend/user/tests.py
@@ -1,3 +1,73 @@
 from django.test import TestCase
+from .models import User
+from config.settings import DEFAULT_GAME_SKIN_IMAGE_URL, DEFAULT_TIER_IMAGE_URL, DEFAULT_GAME_SKIN_IMAGE_URL
+from django.db.utils import IntegrityError
 
-# Create your tests here.
+
+class UserModelTests(TestCase):
+
+    def test_created(self):
+        User.objects.create(
+            email="test@example.com",
+            access_token="someaccesstoken",
+            refresh_token="somerefreshtoken",
+            user_state=True,
+            nickname="test",
+            profile_image_url=DEFAULT_GAME_SKIN_IMAGE_URL,
+            background_color="test",
+            tier=1,  # 예시 값
+            tier_image_url=DEFAULT_TIER_IMAGE_URL,
+            wins=0,  # 예시 값
+            loses=1,  # 예시 값
+            dark_mode=False,
+            game_skin_image_url=DEFAULT_GAME_SKIN_IMAGE_URL,
+            follower_count=0,
+            following_count=0
+        ).save()
+        for user in User.objects.all():
+            print('-----------------------------------------')
+            for attribute, value in vars(user).items():
+                print(f'{attribute} : {value}')
+            print('-----------------------------------------')
+
+    def test_unique_email(self):  # 이메일이 유니크한지 확인
+        # 첫 번째 사용자 생성
+        User.objects.create(
+            email="taeypark@example.com",
+            access_token="someaccesstoken",
+            refresh_token="somerefreshtoken",
+            user_state=True,
+            nickname="Taeypark",
+            profile_image_url=DEFAULT_GAME_SKIN_IMAGE_URL,
+            background_color="Pink",
+            tier=1,  # 예시 값
+            tier_image_url=DEFAULT_TIER_IMAGE_URL,
+            wins=0,  # 예시 값
+            loses=1,  # 예시 값
+            dark_mode=False,
+            game_skin_image_url=DEFAULT_GAME_SKIN_IMAGE_URL,
+            follower_count=0,
+            following_count=0
+        ).save()
+
+        # 다른 이메일로 두 번째 사용자 생성 시도
+        try:
+            User.objects.create(
+                email="taeyparka@example.com",
+                user_state=True,
+                access_token="someaccesstoken",
+                refresh_token="somerefreshtoken",
+                nickname="hyeoan",
+                profile_image_url=DEFAULT_GAME_SKIN_IMAGE_URL,
+                background_color="Pink",
+                tier=1,  # 예시 값
+                tier_image_url=DEFAULT_TIER_IMAGE_URL,
+                wins=1,  # 예시 값
+                loses=0,  # 예시 값
+                dark_mode=False,
+                game_skin_image_url=DEFAULT_GAME_SKIN_IMAGE_URL,
+                follower_count=0,
+                following_count=0
+            ).save()
+        except IntegrityError:
+            self.assertIs(True, False)
