ec179596b7592f6b2a831e7244fe5258449b0b0b
GCgang <powm97@gmail.com>
2024-01-30 14:12:52 +0900

    Feat: relationships 모델 생성
    Co-authored-by: taeypark <ptyolo666@gmail.com>

--- backend/relationships/tests.py
+++ backend/relationships/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Relationship
+from user.models import User
 
-# Create your tests here.
+
+class RelationshipTest(TestCase):
+    def test_follow(self):
+        user1 = User.objects.create(email='hi@hyeoan.com')
+        user2 = User.objects.create(email='yim@yim.com')
+
+        Relationship.objects.create(from_user=user1, to_user=user2)
+        self.assertIn(user2, user1.following.all())
+        self.assertIn(user1, user2.followers.all())
+
+        for rel in user1.following_relationships.all():
+            print(f'{rel.from_user}, {rel.to_user}')
+
+        for rel in user2.follower_relationships.all():
+            print(f'{rel.from_user}, {rel.to_user}')
