36fc565d809d3598a411e79d66b11f9e457cb2e5
GCgang <powm97@gmail.com>
2024-02-06 17:23:47 +0900

    Feat: matches 함수 선언
    Co-authored-by: taeypark <ptyolo666@gmail.com>

--- backend/django/src/matches/views.py
+++ backend/django/src/matches/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from matches.models import Match
 
-# Create your views here.
+def history_handler(request, user_id):
+	pass
+
+def match_handler(request):
+	pass
+
+def in_match_handler(request, match_id):
+	pass
+
