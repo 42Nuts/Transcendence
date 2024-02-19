698856897e1df3a63b4d6cae6002db0d1295dc21
GCgang <powm97@gmail.com>
2024-02-19 15:01:40 +0900

    Fix: 유저 모델 nickname unique=false로 변경
    Co-authored-by: taeypark <ptyolo666@gmail.com>

--- backend/django/src/users/models.py
+++ backend/django/src/users/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from config.settings import DEFAULT_GAME_SKIN_IMAGE_URL, DEFAULT_TIER_IMAGE_URL, DEFAULT_PROFILE_IMAGE_URL
 
 # 커스텀 유저 모델을 생성하고 관리하는 클래스
+
+
 class CustomUserManager(BaseUserManager):
     # 일반 사용자 생성 함수
     def create_user(self, email, password=None, **extra_fields):
@@ -36,20 +38,34 @@ class CustomUserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 # 커스텀 유저 모델 클래스
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField('email address', unique=True)
-    user_state = models.BooleanField(default=True, help_text='Designates whether this user should be treated as active.')
-    nickname = models.CharField(max_length=10, unique=True, help_text='Unique nickname for the user.')
-    profile_image_url = models.ImageField(upload_to='profile/', default=DEFAULT_PROFILE_IMAGE_URL, help_text='URL for the user\'s profile image.')
-    background_color = models.CharField(max_length=20, default='black', help_text='Background color for the user profile.')
-    tier = models.IntegerField(default=0, help_text='User tier, representing their level or rank.')
-    tier_image_url = models.CharField(max_length=255, default=DEFAULT_TIER_IMAGE_URL, help_text='URL for the user\'s tier image.')
-    wins = models.IntegerField(default=0, help_text='Number of wins the user has achieved.')
-    loses = models.IntegerField(default=0, help_text='Number of losses the user has incurred.')
-    dark_mode = models.BooleanField(default=False, help_text='Whether the user prefers dark mode.')
-    game_skin_image_url = models.CharField(max_length=255, default=DEFAULT_GAME_SKIN_IMAGE_URL, help_text='URL for the user\'s game skin image.')
-    follower_count = models.IntegerField(default=0, help_text='Number of followers the user has.')
-    following_count = models.IntegerField(default=0, help_text='Number of users the user is following.')
+    user_state = models.BooleanField(
+        default=True, help_text='Designates whether this user should be treated as active.')
+    nickname = models.CharField(
+        max_length=10, help_text='Unique nickname for the user.')
+    profile_image_url = models.ImageField(
+        upload_to='profile/', default=DEFAULT_PROFILE_IMAGE_URL, help_text='URL for the user\'s profile image.')
+    background_color = models.CharField(
+        max_length=20, default='black', help_text='Background color for the user profile.')
+    tier = models.IntegerField(
+        default=0, help_text='User tier, representing their level or rank.')
+    tier_image_url = models.CharField( 
+        max_length=255, default=DEFAULT_TIER_IMAGE_URL, help_text='URL for the user\'s tier image.')
+    wins = models.IntegerField(
+        default=0, help_text='Number of wins the user has achieved.')
+    loses = models.IntegerField(
+        default=0, help_text='Number of losses the user has incurred.')
+    dark_mode = models.BooleanField(
+        default=False, help_text='Whether the user prefers dark mode.')
+    game_skin_image_url = models.CharField(
+        max_length=255, default=DEFAULT_GAME_SKIN_IMAGE_URL, help_text='URL for the user\'s game skin image.')
+    follower_count = models.IntegerField(
+        default=0, help_text='Number of followers the user has.')
+    following_count = models.IntegerField(
+        default=0, help_text='Number of users the user is following.')
 
     # 사용자를 식별할 때 사용할 필드를 'email'로 설정
     USERNAME_FIELD = 'email'
