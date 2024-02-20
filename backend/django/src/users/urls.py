55bbf13bb24513e26c7d07cfd0206539722e7f79
GCgang <powm97@gmail.com>
2024-02-20 17:17:29 +0900

    Fix: theme api uri 변경
    Co-authored-by: taeypark <ptyolo666@gmail.com>

--- backend/django/src/users/urls.py
+++ backend/django/src/users/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('<int:user_id>/dark-mode/', views.dark_mode_handler),
-    path('<int:user_id>/theme-url/', views.theme_handler),
+    path('<int:user_id>/theme-index/', views.theme_handler),
     path('<int:user_id>/nickname/', views.nickname_handler),
-    path('<int:user_id>/profile-url/', views.profile_url_handler),
+    path('<int:user_id>/profile-index/', views.profile_url_handler),
     path('<int:user_id>/background-color/', views.background_color_handler),
     path('<int:user_id>/tier-url/', views.tier_url_handler),
     path('<int:user_id>/tier/', views.tier_handler),
