67f3374247d7ede7d6912d67f0bf040c5fff5967
euiclee <lechan1317@gmail.com>
2024-02-26 15:45:15 +0900

    Feat: gamePage backicon and play button add #51

--- frontend/src/pages/gamePage.js
+++ frontend/src/pages/gamePage.js
@@ -1,18 +1,27 @@
-import { Component } from "../core/index.js";
+import { Component, createComponent } from "../core/index.js";
+import { BackIconButton, BasicButton } from "../components/Button/index.js";
 
 class GamePage extends Component {
   render() {
     const container = document.createElement("div");
-    const element = document.createElement("h1");
-    element.innerHTML = "Game Page";
+    container.className = "container mx-auto flex flex-col items-center justify-center min-h-screen";
 
-    const anchor = document.createElement("a");
-    anchor.href = "/";
-    anchor.innerText = "Go Home";
+    // grid 레이아웃을 사용하는 컨테이너 생성
+    this.gridContainer = document.createElement("div");
+    this.gridContainer.className = "grid grid-cols-12 gap-10 min-h-screen";
 
-    container.appendChild(element);
-    container.appendChild(anchor);
+    // BackIconButton 추가
+    const backIcon = createComponent(BackIconButton, {});
+    this.gridContainer.appendChild(backIcon);
 
+    // play button
+    const playButtonPos = document.createElement("div");
+    playButtonPos.className = "absolute top-[80%]";
+    const playButton = createComponent(BasicButton, { text: "Play" });
+    playButtonPos.appendChild(playButton);
+
+    container.appendChild(this.gridContainer);
+    container.appendChild(playButtonPos);
     return container;
   }
 }
