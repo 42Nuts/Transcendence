4942e4f6129e026fd3e3ddd6b841e606f4cba09a
gitubanana <ptyolo666@gmail.com>
2024-02-29 15:34:16 +0900

    Feat: websocket 연결
    Co-authored-by: Labin <iys10@naver.com>
    Co-authored-by: PEAKY BLINDERS <powm97@gmail.com>
    
    nginx와 daphne 설정

--- backend/django/src/config/asgi.py
+++ backend/django/src/config/asgi.py
@@ -11,21 +11,18 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+import pongGame.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-import pongGame.routing
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(pongGame.routing.websocket_urlpatterns))
-        ),
+        "websocket": AuthMiddlewareStack(URLRouter(pongGame.routing.websocket_urlpatterns)),
     }
 )
