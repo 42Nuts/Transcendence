92fb3e7d0c4bcc0b5e8594120ea426b8ed7502d7
yim <iys10@naver.com>
2024-03-06 13:48:12 +0900

    Feat: group play only 1 game
    Co-authored-by: taeypark <ptyolo666@gmail.com>
    Co-authored-by: PEAKY BLINDERS <powm97@gmail.com>

--- backend/django/src/pongGame/consumers.py
+++ backend/django/src/pongGame/consumers.py
@@ -8,27 +8,28 @@ from asgiref.sync import async_to_sync
 
 logger = logging.getLogger('django')
 
+group_game_instances = {}
+
 class GameConsumer(AsyncWebsocketConsumer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        logger.info('hihi sun')
-        self.game = PongGame()
-        self.update_task = None
-
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'game_%s' % self.room_name
 
-        logger.info('hihi yim')
+        # 그룹에 대한 게임 인스턴스가 존재하지 않으면 생성
+        if self.room_group_name not in group_game_instances:
+            group_game_instances[self.room_group_name] = PongGame()
+ 
+        # 게임 인스턴스를 현재 소비자 인스턴스에 할당
+        self.game = group_game_instances[self.room_group_name]
+
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
-        logger.info('websocket connect')
         await self.accept()
-        self.update_task = asyncio.ensure_future(self.game_update_task())
+        # 게임 업데이트 작업 시작
+        self.update_task = asyncio.create_task(self.game_update_task())
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
@@ -38,9 +39,10 @@ class GameConsumer(AsyncWebsocketConsumer):
         if self.update_task:
             self.update_task.cancel()
 
+    # 게임 상태 업데이트 및 그룹에 전송
     async def game_update_task(self):
         while True:
-            await asyncio.sleep(0.01)  # 화면 업데이트 주기
+            await asyncio.sleep(0.01)  # 게임 상태 업데이트 주기
             game_data = self.game.update()  # 게임 상태 업데이트
             await self.channel_layer.group_send(
                 self.room_group_name,
@@ -52,9 +54,8 @@ class GameConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         user_input = json.loads(text_data)
-        self.game.update(user_input)  # 사용자 입력에 따라 게임 상태 업데이트 (화면에는 즉시 반영되지 않음)
+        self.game.update(user_input)  # 사용자 입력에 따른 게임 상태 업데이트
 
     async def game_update(self, event):
         game_data = event['game_data']
-        # WebSocket을 통해 게임 데이터 전송
         await self.send(text_data=json.dumps(game_data))
