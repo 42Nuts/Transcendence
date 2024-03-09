a2d463bf82195a3a28fe390da8a70e9132b51755
Eui Lee <lechan1317@gmail.com>
2024-03-09 18:20:47 +0900

    Feat: dark mode func add

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
