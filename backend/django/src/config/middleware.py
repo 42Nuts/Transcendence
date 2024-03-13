33733a9a8b0967281239b3895f5b5c456864f71c
gitubanana <ptyolo666@gmail.com>
2024-03-13 10:39:12 +0900

    Feat: django healthCheck 기능 추가
    Co-authored-by: PEAKY BLINDERS <powm97@gmail.com>

--- backend/django/src/config/middleware.py
+++ backend/django/src/config/middleware.py
@@ -28,6 +28,10 @@ def tokenCheck(get_response):
         logger.info("Request GET: %s", str(request.GET))
         logger.info("Request Headers: %s", str(request.headers))
 
+        if request.path == '/health/':
+            response = get_response(request)
+            return response
+
         try:
             # 토큰 검사 (토큰 재발급 로직 필요)
             user = get_user(request)
