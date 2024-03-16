f91c0d1fd4edccfab3e466fdf359dc70ca97a3cb
Eui Lee <lechan1317@gmail.com>
2024-03-16 17:58:13 +0900

    Feat: move button function add #112

--- frontend/src/components/Button/moveButton.js
+++ frontend/src/components/Button/moveButton.js
@@ -1,9 +1,51 @@
 import { Component } from "../../core/index.js";
 
 class MoveButton extends Component {
+  constructor(props) {
+    super(props);
+
+    this.points = [];
+  }
+
+  movePointRight() {
+    const firstPoint = this.points.shift();
+    this.points.push(firstPoint);
+
+    this.updatePointsClasses();
+  }
+
+  movePointLeft() {
+    const lastPoint = this.points.pop();
+    this.points.unshift(lastPoint);
+
+    this.updatePointsClasses();
+  }
+
+  updatePointsClasses() {
+    this.points.forEach((point) => {
+      point.className = "w-2 h-2 bg-primary-inner_card_top rounded-full";
+    });
+
+    this.points[0].className = "w-[9px] h-[9px] bg-primary-text rounded-full";
+  }
+
   render() {
     const container = document.createElement("div");
     container.className = "justify-start items-center gap-4 inline-flex";
+
+    // point
+    const buttonsContainer = document.createElement("div");
+    buttonsContainer.className = "justify-center items-center gap-1.5 flex";
+    
+    for (let i = 0; i < 5; i++) {
+      const point = document.createElement("div");
+      point.className =
+      i === 0
+      ? "w-[9px] h-[9px] bg-primary-text rounded-full"
+      : "w-2 h-2 bg-primary-inner_card_top rounded-full";
+      this.points.push(point);
+      buttonsContainer.appendChild(point);
+    }
 
     // left arrow
     const leftButton = document.createElement("button");
@@ -15,30 +57,7 @@ class MoveButton extends Component {
 
     leftButton.appendChild(leftImage);
 
-    // point
-    const buttonsContainer = document.createElement("div");
-    buttonsContainer.className = "justify-center items-center gap-1.5 flex";
-
-    const point1 = document.createElement("div");
-    point1.className = "w-[9px] h-[9px] bg-primary-text rounded-full";
-
-    const point2 = document.createElement("div");
-    point2.className = "w-2 h-2 bg-primary-inner_card_top rounded-full";
-
-    const point3 = document.createElement("div");
-    point3.className = "w-2 h-2 bg-primary-inner_card_top rounded-full";
-
-    const point4 = document.createElement("div");
-    point4.className = "w-2 h-2 bg-primary-inner_card_top rounded-full";
-
-    const point5 = document.createElement("div");
-    point5.className = "w-2 h-2 bg-primary-inner_card_top rounded-full";
-
-    buttonsContainer.appendChild(point1);
-    buttonsContainer.appendChild(point2);
-    buttonsContainer.appendChild(point3);
-    buttonsContainer.appendChild(point4);
-    buttonsContainer.appendChild(point5);
+    leftButton.addEventListener("click", () => this.movePointLeft());
 
     // right arrow
     const rightButton = document.createElement("button");
@@ -49,6 +68,8 @@ class MoveButton extends Component {
     rightButton.className = "w-[18px] h-[18px] relative";
 
     rightButton.appendChild(rightImage);
+
+    rightButton.addEventListener("click", () => this.movePointRight());
 
     container.appendChild(leftButton);
     container.appendChild(buttonsContainer);
