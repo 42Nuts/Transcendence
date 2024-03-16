1fe3db5c6f4e8ff805063316de6123ceb6009507
Eui Lee <lechan1317@gmail.com>
2024-03-16 10:25:07 +0900

    Init: profilepage js #112

--- frontend/src/pages/profilePage.js
+++ frontend/src/pages/profilePage.js
@@ -1,10 +1,18 @@
 import { Component } from "../core/index.js";
+import Store from "../store/index.js";
 
 class ProfilePage extends Component {
   render() {
-    const container = document.createElement("div");
+    this.container = document.createElement("div");
+    this.container.className = "flex items-center justify-center min-h-screen bg-no-repeat bg-cover";
+    if (Store.state.darkMode) {
+      this.container.style.backgroundImage = 'url("/static/assets/images/net-dark.svg")';
+    } else {
+      this.container.style.backgroundImage = 'url("/static/assets/images/net.svg")';
+    }
+    this.container.style.backgroundPosition = "calc(100% + 390px) center";
 
-    return container;
+    return this.container;
   }
 }
 
