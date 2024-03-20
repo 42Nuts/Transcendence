6ffeedf3c4b83dacf4731a4e102b7e8918e11d4c
yim <iys10@naver.com>
2024-03-20 17:19:07 +0900

    Refactor: tmp

--- frontend/src/pages/gamePage.js
+++ frontend/src/pages/gamePage.js
@@ -17,12 +17,12 @@ class GamePage extends Component {
     const container = document.createElement("div");
     console.log("Store theme", Store.state.theme);
 
-    this.load = createComponent(Loading, {});
+    // this.load = createComponent(Loading, {});
 
     const game = createComponent(TwoPlayerMode, {});
 
     container.appendChild(game);
-    container.appendChild(this.load);
+    // container.appendChild(this.load);
 
     return container;
   }
