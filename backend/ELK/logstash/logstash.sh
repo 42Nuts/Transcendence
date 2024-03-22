af461c69259e8a2fc9bad700a767ade2d6d58589
GCgang <powm97@gmail.com>
2024-03-22 18:59:55 +0900

    Feat: logstash file 패턴, 필터 추가
    Co-authored-by: taeypark <ptyolo666@gmail.com>

--- backend/ELK/logstash/logstash.sh
+++ backend/ELK/logstash/logstash.sh
@@ -13,6 +13,10 @@ input {
         path => [ '/usr/share/logstash/logs/django.log' ]
         start_position => 'beginning'
     }
+
+    file {
+        type => 'postgresql'
+        path => [ '/usr/share/logstash/logs/postgresql.log' ]
         start_position => 'beginning'
     }
 }
@@ -28,14 +32,18 @@ filter {
             match => { 'message' => '%{LOGLEVEL:loglevel} %{TIMESTAMP_ISO8601:timestamp},%{NUMBER:microseconds} %{WORD:component} %{NUMBER:process_id} %{NUMBER:thread_id} %{DATA:request_info}: %{GREEDYDATA:request_detail}' }
         }
     }
+    else if [type] == 'postgresql' {
+        grok {
+            match => { 'message' => '%{TIMESTAMP_ISO8601:timestamp} %{DATA:timezone} [%{NUMBER:process_id}] %{WORD:log_level}:  %{GREEDYDATA:message}' }
+        }
     }
 }
 
 output {
     elasticsearch {
-        hosts => [ 'http://elasticsearch:9200' ]
+        hosts  => [ 'http://elasticsearch:9200' ]
     }
 }
 " > ./logstash.conf
 
-logstash -f /logstash.conf
+logstash -f ./logstash.conf
