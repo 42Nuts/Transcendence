e53d92272fe29bd20083f0e86ee2c81f34d6de27
euiclee <lechan1317@gmail.com>
2024-03-22 10:57:05 +0900

    Feat: nginx error page add
    Co-authored-by: taeypark <ptyolo666@gmail.com>

--- backend/nginx/nginx.sh
+++ backend/nginx/nginx.sh
@@ -14,6 +14,8 @@ http {
     server {
         listen $NGINX_PORT ssl;
 
+        error_page 404 /assets/static/404.html;
+
         ssl_certificate /etc/nginx/certificate.crt;
         ssl_certificate_key /etc/nginx/private.key;
         ssl_protocols TLSv1.2 TLSv1.3;
@@ -24,6 +26,7 @@ http {
         }
 
         location / {
+          proxy_intercept_errors on;
           proxy_pass http://$DJANGO_HOST:$DJANGO_PORT;
         }
 
@@ -33,6 +36,10 @@ http {
           proxy_set_header Upgrade \$http_upgrade;
           proxy_set_header Connection "Upgrade";
           proxy_set_header Host \$host;
+        }
+
+        location /assets/static/404.html {
+          root /var/www/html;
         }
 
         location /metrics {
