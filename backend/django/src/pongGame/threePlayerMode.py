df36424eb37580525f03ea7085c4d00c1b0eb131
yim <iys10@naver.com>
2024-03-22 15:02:07 +0900

    Fix: 3인 공 속도 조절

--- backend/django/src/pongGame/threePlayerMode.py
+++ backend/django/src/pongGame/threePlayerMode.py
@@ -104,9 +104,9 @@ class threePlayer:
             x = self.canvas.width / 2,
             y = self.canvas.height - self.canvas.width / (2 * (3 ** 0.5)),
             radius = 15,
-            velocity_x = 3 * math.cos(math.radians(self.reset_angle)),
-            velocity_y = 3 * math.sin(math.radians(self.reset_angle)),
-            speed = 3,
+            velocity_x = 2 * math.cos(math.radians(self.reset_angle)),
+            velocity_y = 2 * math.sin(math.radians(self.reset_angle)),
+            speed = 2,
             color = "WHITE"
         )
 
@@ -117,7 +117,7 @@ class threePlayer:
 
         self.ball.x = self.canvas.width / 2
         self.ball.y = self.canvas.height - self.canvas.width / (2 * (3 ** 0.5))
-        self.ball.speed = 3
+        self.ball.speed = 2
         self.last_touch_player = None
 
         angle_radians = math.radians(self.reset_angle)
