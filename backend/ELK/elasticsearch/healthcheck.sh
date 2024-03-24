af94bbde7a8027f5d1097530f5b1bcee0f8b77b7
gitubanana <ptyolo666@gmail.com>
2024-03-24 18:49:19 +0900

    Chore: http_ca.crt 바인드 마운트
    Co-authored-by: GCgang <powm97@gmail.com>

--- backend/ELK/elasticsearch/healthcheck.sh
+++ backend/ELK/elasticsearch/healthcheck.sh
@@ -5,5 +5,7 @@ if [ -f /tmp/isOk ]; then \
 else \
     bin/elasticsearch-create-enrollment-token -s kibana > ./token && \
     bin/elasticsearch-reset-password -u $ELASTICSEARCH_USER -i < ./password && \
+    cat /usr/share/elasticsearch/config/certs/http_ca.crt > /usr/share/elasticsearch/http_ca.crt && \
+    hostname > /usr/share/elasticsearch/hostname && \
     touch /tmp/isOk; \
 fi
