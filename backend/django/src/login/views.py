c40f6486fc0405bbf2ddbc86b2ef71ca3338a9c7
GCgang <powm97@gmail.com>
2024-03-26 18:50:01 +0900

    Feat: request_oauth_token 함수 추가
    Co-authored-by: taeypark <ptyolo666@gmail.com>

--- backend/django/src/login/views.py
+++ backend/django/src/login/views.py
@@ -20,7 +20,23 @@ def get_tokens_for_user(user):
         'access': str(refresh.access_token)
     }
 
-
+# OAuth 서버에서 엑세스 토큰 및 리프레시 토큰을 요청하는 함수
+def request_oauth_token(authorization_code):
+    # 엑세스 토큰 및 리프레시 토큰을 요청하는 URI
+    token_url = os.environ.get('FOURTYTWO_TOKEN_URI')
+    # 인증 코드를 이용해 엑세스 토큰 요청 시 사용할 payload
+    payload = {
+        'grant_type': os.environ.get('FOURTYTWO_GRANT_TYPE'),
+        'client_id': os.environ.get('FOURTYTWO_CLIENT_ID'),
+        'client_secret': os.environ.get('FOURTYTWO_CLIENT_SECRET'),
+        'redirect_uri': os.environ.get('FOURTYTWO_REDIRECT_URI'),
+        'code': authorization_code,
+    }
+    # 엑세스 토큰 요청
+    token_response = requests.post(token_url, data=payload)
+    # HTTP 요청 실패 시 예외 발상
+    token_response.raise_for_status()
+    return token_response.json()
 def fourtytwo_oauth(request):
     if request.method != 'GET':
         return JsonResponse({'status': 403})
