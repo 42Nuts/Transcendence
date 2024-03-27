bdc169f0bc39ddcc14eb7f04a8d978433aae9cb9
yim <iys10@naver.com>
2024-03-27 15:11:05 +0900

    Feat: onePlayerMode

---
+++ backend/django/src/pongGame/onePlayerMode.py
@@ -0,0 +1,209 @@
+import asyncio
+import json
+import math
+
+class GameCanvas:
+    def __init__(self, width, height, paddle_length):
+        self.width = width
+        self.height = height
+        self.paddle_length = paddle_length
+
+class GameBall:
+    def __init__(self, x, y, radius, velocity_x, velocity_y, speed, color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.velocity_x = velocity_x
+        self.velocity_y = velocity_y
+        self.speed = speed
+        self.color = color
+
+    def to_dict(self):
+        return {
+            "x": self.x, 
+            "y": self.y, 
+            "radius": self.radius, 
+            "color": self.color
+        }
+
+class Paddle:
+    def __init__(self, x, y, width, height, score, color, leftArrow, rightArrow, a_press, d_press, id, angle):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.score = score
+        self.color = color
+        self.leftArrow = leftArrow  
+        self.rightArrow = rightArrow
+        self.a_press = a_press
+        self.d_press = d_press
+        self.id = id
+        self.angle = angle
+
+    def to_dict(self):
+        return {
+            "x": self.x, 
+            "y": self.y, 
+            "score": self.score, 
+            "width": self.width, 
+            "height": self.height, 
+            "color": self.color,
+            "angle": self.angle,
+            "playerId": self.id
+        }
+
+class onePlayer:
+    def __init__(self, player_ids):
+        self.canvas = GameCanvas(width=700, height=700, paddle_length=100)
+        self.players = []
+        self.winner = None
+
+        self.players.append(Paddle(
+            x=(self.canvas.width - self.canvas.paddle_length) / 2,
+            y=self.canvas.height - 20,
+            width=100,
+            height=10,
+            score=0,
+            color="white",
+            leftArrow=False,
+            rightArrow=False,
+            a_press=False,
+            d_press=False,
+            angle = 0,
+            id=player_ids[0]
+        ))
+
+        self.players.append(Paddle(
+            x=(self.canvas.width - self.canvas.paddle_length) / 2,
+            y=10,
+            width=100,
+            height=10,
+            score=0,
+            color="WHITE",
+            leftArrow=False,
+            rightArrow=False,
+            a_press=False,
+            d_press=False,
+            angle = 180,
+            id=player_ids[1]
+        ))
+
+        self.player_map = {player.id: player for player in self.players}
+
+        self.ball = GameBall(
+            x=self.canvas.width / 2,
+            y=self.canvas.height / 2,
+            radius=15,
+            velocity_x=2,
+            velocity_y=2,
+            speed=4,
+            color="WHITE"
+        )
+
+    def reset_ball(self):
+        self.ball.x = self.canvas.width / 2
+        self.ball.y = self.canvas.height / 2
+        self.ball.velocity_x = -2 if self.ball.velocity_x > 0 else 2
+        self.ball.velocity_y = -2 if self.ball.velocity_y > 0 else 2
+        self.ball.speed = 4
+
+    def collision(self, b, p):
+        p.top = p.y
+        p.bottom = p.y + p.height
+        p.left = p.x
+        p.right = p.x + p.width
+
+        b.top = b.y - b.radius
+        b.bottom = b.y + b.radius
+        b.left = b.x - b.radius
+        b.right = b.x + b.radius
+
+        return p.left < b.right and p.top < b.bottom and p.right > b.left and p.bottom > b.top
+
+    def update_player_movement(self, index, player):
+        if player.leftArrow and player.rightArrow:
+            return
+        if player.a_press and player.d_press:
+            return
+        if index == 0:
+            if player.leftArrow and player.x > 0:
+                player.x -= 8
+            if player.rightArrow and player.x < self.canvas.width - player.width:
+                player.x += 8
+        if index == 1:
+            if player.a_press and player.x > 0:
+                player.x -= 8
+            if player.d_press and player.x < self.canvas.width - player.width:
+                player.x += 8
+    
+    def check_winner(self):
+        for player in self.players:
+            if player.score >= 5:
+                self.winner = player.id
+                return True
+        return False
+
+    def update(self, user_input=None):
+        # score
+        if self.ball.y - self.ball.radius < 0:
+            self.players[0].score += 1
+            self.reset_ball()
+        elif self.ball.y + self.ball.radius > self.canvas.height:
+            self.players[1].score += 1
+            self.reset_ball()
+        
+        # 공의 위치 변경
+        self.ball.x += self.ball.velocity_x
+        self.ball.y += self.ball.velocity_y
+
+        # 공의 벽 튕김
+        if self.ball.x - self.ball.radius < 0:
+            self.ball.velocity_x = -self.ball.velocity_x
+            self.ball.x = self.ball.radius 
+        elif self.ball.x + self.ball.radius > self.canvas.width:
+            self.ball.velocity_x = -self.ball.velocity_x
+            self.ball.x = self.canvas.width - self.ball.radius
+
+        # 공의 위치에 따른 플레이어 확인
+        player = self.players[0] if self.ball.y + self.ball.radius > self.canvas.height / 2 else self.players[1]
+
+        # 플레이어가 누군지에 따라 공이 중앙에 가까울 수록 0도, 끝에 가까울 수록 45도로 튕기게 설정
+        if self.collision(self.ball, player):
+            collidePoint = (self.ball.x - (player.x + player.width / 2))
+            collidePoint /= (player.width / 2)
+
+            angleRad = (math.pi / 4) * collidePoint
+
+            direction = 1 if self.ball.y + self.ball.radius < self.canvas.height / 2 else -1
+
+            # 스피드를 공의 움직임에 적용
+            self.ball.velocity_x = self.ball.speed * math.sin(angleRad)
+            self.ball.velocity_y = direction * self.ball.speed * math.cos(angleRad)
+
+            # 게임 중 스피드가 점차 증가
+            self.ball.speed += 0.1
+
+        # 키보드 입력에 따른 변수 변화
+        if user_input:
+            player_id = user_input.get("playerId")
+            player = self.player_map.get(player_id)
+
+            if player:
+                player.leftArrow = user_input.get("leftArrow", False)
+                player.rightArrow = user_input.get("rightArrow", False)
+                player.a_press = user_input.get("a_press", False)
+                player.d_press = user_input.get("d_press", False)
+
+                # 두 키가 동시에 눌렸을 경우 움직임 없음
+        for index, player in enumerate(self.players):
+            self.update_player_movement(index, player)
+
+        self.check_winner()
+        # 프론트엔드에 필요한 정보 보내기
+        return {
+            "winner": self.winner,
+            "ball": self.ball.to_dict(),
+            "players": [player.to_dict() for player in self.players]
+        }
+
---
+++ frontend/src/game/onePlayerMode.js
@@ -0,0 +1,337 @@
+import { Component, createComponent } from "../core/index.js";
+import { BasicButton } from "../components/Button/index.js";
+import { ScoreBoard } from "../components/Board/index.js";
+import { mapImages, themeImages, profileImages } from "../config/index.js";
+import { Result } from "./Result/index.js";
+import { Countdown } from "./Loading/index.js";
+import Store from "../store/index.js";
+
+class onePlayerMode extends Component {
+  constructor(props) {
+    super(props);
+    this.result = false;
+    this.keyState = {
+      leftArrow: false,
+      rightArrow: false,
+      a_press: false,
+      d_presss: false,
+    };
+    this.backGround = new Image();
+    this.backGround.src = mapImages[Store.state.theme];
+
+    this.ballImage = new Image();
+    this.ballImage.src = themeImages[Store.state.theme];
+  }
+
+  showResult(message) {
+    const overlay = createComponent(Result, { result: message });
+    overlay.setAttribute("href", "/gameMode/");
+
+    if (!this.result) {
+      document.body.appendChild(overlay);
+    }
+
+    overlay.addEventListener("click", () => {
+      document.body.removeChild(overlay);
+      this.result = false;
+    });
+
+    this.result = true;
+  }
+
+  renderGame(data) {
+    // draw backGround
+    if (this.backGround.complete) {
+      this.ctx.drawImage(
+        this.backGround,
+        0,
+        0,
+        this.canvas.width,
+        this.canvas.height
+      );
+    } else {
+      this.backGround.onload = function () {
+        this.ctx.drawImage(
+          this.backGround,
+          0,
+          0,
+          this.canvas.width,
+          this.canvas.height
+        );
+      };
+    }
+
+    let player;
+
+    this.ctx.save();
+    for (var i = 0; i < data.players.length; i++) {
+      if (userId == data.players[i].playerId) {
+        player = data.players[i];
+      }
+    }
+
+    for (var i = 0; i < data.players.length; i++) {
+      this.drawRect(
+        data.players[i].x,
+        data.players[i].y,
+        data.players[i].width,
+        data.players[i].height,
+        data.players[i].color
+      );
+    }
+
+    // draw the ball
+    if (this.ballImage.complete) {
+      this.ctx.drawImage(
+        this.ballImage,
+        data.ball.x - data.ball.radius,
+        data.ball.y - data.ball.radius,
+        data.ball.radius * 2,
+        data.ball.radius * 2
+      );
+    } else {
+      this.ballImage.onload = function () {
+        this.ctx.drawImage(
+          this.ballImage,
+          data.ball.x - data.ball.radius,
+          data.ball.y - data.ball.radius,
+          data.ball.radius * 2,
+          data.ball.radius * 2
+        );
+      };
+    }
+
+    this.ctx.restore();
+
+    if (data.winner) {
+      if (data.winner == userId) {
+        this.showResult("win");
+      } else {
+        this.showResult("lose");
+      }
+      //tournamet 인지 확인
+      this.gameSocket.close();
+      this.destroy();
+    }
+  }
+
+  keyboardEvent() {
+    this.handleKeyDown = (event) => {
+      if (event.keyCode === 37) {
+        this.keyState.leftArrow = true;
+      } else if (event.keyCode === 39) {
+        this.keyState.rightArrow = true;
+      } else if (event.keyCode == 65) {
+        this.keyState.a_press = true;
+      } else if (event.keyCode == 68) {
+        this.keyState.d_press = true;
+      }
+      this.gameSocket.send(
+        JSON.stringify({ playerId: userId, ...this.keyState })
+      );
+    };
+
+    this.handleKeyUp = (event) => {
+      if (event.keyCode === 37) {
+        this.keyState.leftArrow = false;
+      } else if (event.keyCode === 39) {
+        this.keyState.rightArrow = false;
+      } else if (event.keyCode == 65) {
+        this.keyState.a_press = false;
+      } else if (event.keyCode == 68) {
+        this.keyState.d_press = false;
+      }
+      this.gameSocket.send(
+        JSON.stringify({ playerId: userId, ...this.keyState })
+      );
+    };
+
+    // 키보드 이벤트 리스너 연결
+    document.addEventListener("keydown", this.handleKeyDown);
+    document.addEventListener("keyup", this.handleKeyUp);
+  }
+
+  removeKeyboardEvent() {
+    document.removeEventListener("keydown", this.handleKeyDown);
+    document.removeEventListener("keyup", this.handleKeyUp);
+  }
+
+
+
+  drawText(text, x, y) {
+    this.ctx.fillStyle = "BLACK";
+    this.ctx.font = "60px 바탕";
+    this.ctx.fillText(text, x, y);
+  }
+
+  drawArc(x, y, r, color) {
+    this.ctx.fillStyle = color;
+    this.ctx.beginPath();
+    this.ctx.arc(x, y, r, 0, Math.PI * 2, true);
+    this.ctx.closePath();
+    this.ctx.fill();
+  }
+
+  drawRect(x, y, w, h, color) {
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect(x, y, w, h);
+  }
+
+  updateScore(players) {
+    const player1 = document.getElementById("Score1");
+    const player2 = document.getElementById("Score2");
+
+    if (player1 && player2) {
+      player1.innerText = players[0].score;
+      player2.innerText = players[1].score;
+    }
+  }
+
+  updateScoreBoard(playerIds) {
+    playerIds.forEach((playerId, index) => {
+      Promise.all([
+        fetch(`/v2/users/${playerId}/profile-index/`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }).then((response) => response.json()),
+        fetch(`/v2/users/${playerId}/nickname/`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }).then((response) => response.json()),
+      ])
+        .then((data) => {
+          const [profileResponse, nicknameResponse] = data;
+          const playerImage = document.getElementById(`Img${index + 1}`);
+          const playerName = document.getElementById(`Name${index + 1}`);
+          if (playerImage && playerName) {
+            playerImage.src = profileImages[profileResponse.profile_index];
+            playerName.innerText = nicknameResponse.nickname;
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching player data:", error);
+        });
+    });
+  }
+
+  initializeGame() {
+    this.canvas.width = 700;
+    this.canvas.height = 700;
+
+    this.ctx = this.canvas.getContext("2d");
+
+    this.gameSocket = new WebSocket(
+      "wss://" + window.location.host + `/ws/game/?mode=2p&userId=${userId}`
+    );
+
+    this.gameSocket.onmessage = (e) => {
+      const data = JSON.parse(e.data);
+
+      if (data.type == "game_end") {
+        this.showResult("win");
+        this.gameSocket.close();
+        this.destroy();
+        return;
+      } else if (data.type == "game_start") {
+        this.updateScoreBoard(data.player_ids);
+        Store.dispatch("updateGameStart");
+        document.body.appendChild(createComponent(Countdown, {}));
+        this.keyboardEvent();
+      } else {
+        this.renderGame(data);
+        this.updateScore(data.players);
+      }
+    };
+
+    this.gameSocket.onclose = function (e) {
+      console.log("Game socket closed");
+    };
+
+    this.handlePopState = this.closeWebSocketOnBack.bind(this);
+    window.addEventListener("popstate", this.handlePopState);
+  }
+
+  closeWebSocketOnBack() {
+    if (this.gameSocket && this.gameSocket.readyState === WebSocket.OPEN) {
+      this.gameSocket.close();
+    }
+  }
+
+  destroy() {
+    // 필요한 경우 페이지를 벗어날 때 호출
+    window.removeEventListener("popstate", this.handlePopState);
+    this.removeKeyboardEvent();
+  }
+
+  render() {
+    this.container = document.createElement("div");
+    this.container.className = "flex items-center justify-center min-h-screen";
+
+    // game container
+    this.gameContainer = document.createElement("div");
+    this.gameContainer.className = "relative";
+
+    // score board
+    this.scoreContainer = document.createElement("div");
+    this.scoreContainer.className =
+      "absolute w-44 h-44 flex-col justify-start items-start gap-8 inline-flex";
+    this.scoreContainer.style = "left: 100%; top:0; margin-left: 64px;";
+
+    this.player1 = createComponent(ScoreBoard, {
+      imgSrc: "/static/assets/images/profile-default.svg",
+      tierSrc: "/static/assets/images/tier-bronze.svg",
+      name: "Player 1",
+      id: "1",
+    });
+
+    this.player2 = createComponent(ScoreBoard, {
+      imgSrc: "/static/assets/images/profile-default.svg",
+      tierSrc: "/static/assets/images/tier-bronze.svg",
+      name: "Player 2",
+      id: "2",
+    });
+
+    this.scoreContainer.appendChild(this.player1);
+    this.scoreContainer.appendChild(this.player2);
+
+    // canvas to draw the game
+    this.canvas = document.createElement("canvas");
+    this.canvas.className =
+      "rounded-3xl border-8 border-primary-card_background dark:border-secondary-card_background shadow-md";
+
+    this.initializeGame();
+
+    // button to exit the game
+    this.exitButtonPos = document.createElement("div");
+    this.exitButtonPos.className = "absolute top-[85%]";
+
+    this.exitButtonHref = document.createElement("a");
+    this.exitButtonHref.setAttribute("href", "/gameMode/");
+
+    this.exitButton = createComponent(BasicButton, { text: "Exit" });
+
+    this.exitButtonHref.appendChild(this.exitButton);
+    this.exitButtonPos.appendChild(this.exitButtonHref);
+
+    this.gameContainer.appendChild(this.canvas);
+    this.gameContainer.appendChild(this.scoreContainer);
+    this.container.appendChild(this.gameContainer);
+    this.container.appendChild(this.exitButtonPos);
+
+    this.exitButton.addEventListener("click", () => {
+      // WebSocket 연결이 열려있다면 종료
+      if (this.gameSocket && this.gameSocket.readyState === WebSocket.OPEN) {
+        this.gameSocket.close();
+        console.log("WebSocket connection closed.");
+      }
+    });
+
+    return this.container;
+  }
+}
+
+export default onePlayerMode;
