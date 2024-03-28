cdf0a62221fb78b2a923748b7a0de9e3b9f7732c
GCgang <powm97@gmail.com>
2024-03-28 11:00:45 +0900

    Fix: django unhealthy 에러 해결
    Co-authored-by: taeypark <ptyolo666@gmail.com>

--- backend/django/src/config/asgi.py
+++ backend/django/src/config/asgi.py
@@ -12,13 +12,13 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import pongGame.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
+import pongGame.routing
 
 application = ProtocolTypeRouter(
     {
